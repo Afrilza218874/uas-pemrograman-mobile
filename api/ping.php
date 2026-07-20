91e975723f60120a9e26fbc04781db4ccb14c964
Afrilza.maulid <afrilza.maulid123@gmail.com>
2026-07-20 18:45:49 +0700

    Delete api/ping.php

--- api/ping.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../helpers.php';
-
-// Endpoint publik khusus untuk pengujian / screenshot laporan (tanpa Auth)
-successResponse("Ini adalah contoh balasan REST API yang berhasil dimuat.", [
-    "aplikasi" => "NewsClip UAS",
-    "status" => "Berjalan Normal",
-    "koneksi_database" => "Terhubung"
-]);
